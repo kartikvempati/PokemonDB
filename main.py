604a108040e8c2690f39f3b81f325ebe20bad40c
Kartik Vempati <kvempati11@gmail.com>
2015-08-25 22:03:16 -0700

    Fix Query errors. Add duplicate entry check for put/add

--- main.py
+++ main.py
@@ -3,21 +3,31 @@ import pokemon
 import pokeDB
 
 db = pokeDB.PokeDB()
-cur = db.create_Table()
+cur, conn = db.create_Table()
 
-bulbasaur = pokemon.Pokemon(1, "Bulbasaur", "Grass", "Poison")
-print(bulbasaur.prim)
+bulbasaur = pokemon.Pokemon(1, 'Bulbasaur', 'Grass', 'Poison')
+
 charmander = pokemon.Pokemon(5, "Charmander", "Fire")
-print(charmander.sec)
 charmander.set_ID(4)
+
 squirtle = pokemon.Pokemon(7, "Squirtles", "Water", "Ground")
 squirtle.set_Secondary()
 
-print(squirtle.as_dict())
 
 
-db.put(bulbasaur,cur)
-# db.put(charmander,cur)
+db.put(bulbasaur, cur)
+db.put(charmander, cur)
+db.put(squirtle, cur)
+
+charmander.set_ID(6)
+charmander.set_Name('Charizard')
+charmander.set_Secondary('Flying')
+
+db.update(charmander, 4, cur)
+
+cur.execute('''SELECT * FROM Pokemon''')
+row = cur.fetchall()
+print(row)
 
 
 
--- pokeDB.py
+++ pokeDB.py
@@ -1,6 +1,6 @@
 import MySQLdb as sql
 import pokemon
-from _mysql_exceptions import OperationalError
+import _mysql_exceptions
 
 
 class PokeDB():
@@ -9,20 +9,30 @@ class PokeDB():
 		cur = conn.cursor()
 		try: 
 			cur.execute ("CREATE TABLE Pokemon (ID INT PRIMARY KEY, name VARCHAR(255), Type1 VARCHAR(255), Type2 VARCHAR(255))")
-		except OperationalError:
+		except _mysql_exceptions.OperationalError:
 			print("Pokemon was already created")
-		return cur
+		return cur, conn
 
-	def get(self,id,cur):
+	def get(self,id, cur):
 		cur.execute("SELECT * FROM Pokemon WHERE ID = %s"%str(id))
 		row = cur.fetchone()
-		return row
+		return row 
 
 	def put(self,Pokemon,cur):
-		cur.execute("INSERT INTO Pokemon (ID, name, Type1, Type2) VALUES (Pokemon.id, Pokemon.name, Pokemon.prim, Pokemon.sec)")
+		try: 
+			cur.execute('''INSERT INTO Pokemon (ID, name, Type1, Type2) VALUES (%s, %s, %s, %s)''',(Pokemon.id, Pokemon.name, Pokemon.prim, Pokemon.sec))
+		except _mysql_exceptions.IntegrityError:
+			print("That is a duplicate entry!")
+			return False
+		return True
 
 	def update(sef,Pokemon, id,cur):
-		cur.execute("UPDATE Pokemon SET name = Pokemon.name, Type1 = Pokemon.prim, Type2 = Pokemon.sec WHERE ID = Pokemon.id")
-
+		try:
+			cur.execute('''UPDATE Pokemon SET ID = %s, name = %s, Type1 = %s, Type2 = %s WHERE ID = %s''',(Pokemon.id, Pokemon.name, Pokemon.prim, Pokemon.sec, id))
+		except _mysql_exceptions.IntegrityError:
+			print("That is a duplicate entry!")
+			return False
+		return True
 	def delete(self,Pokemon,cur):
-		cur.execute("DELETE FROM Pokemon where ID = Pokemon.id")
+		cur.execute('''DELETE FROM Pokemon where ID = %s''', (Pokemon.id))
+
